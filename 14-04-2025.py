95ceea61f81bad93bca6b59bac616666895e6a15
joffrey7486 <joffreycaugnon@gmail.com>
2025-04-15 17:48:14 +0200

    create random array and element

--- 14-04-2025.py
+++ 14-04-2025.py
@@ -1,3 +1,11 @@
+import random
+
+# Créer un tableau de nombre unique aléatoire
+def create_unique_random_array(size, max_val=100):
+    return random.sample(range(max_val), size)
+
+
+
 # Retrouve l'index de l'élément dans le tableau
 def find_index(array, element):
     for i in range(len(array)):
@@ -5,4 +13,14 @@ def find_index(array, element):
             return i
     return None  
 
-print(find_index([13, 18, 25, 2, 8, 10], 10))
+arr = create_unique_random_array(10, 10)
+my_number = random.randint(1, 10)
+index = find_index(arr, my_number)
+
+print("Begin")
+
+print("My array: {}".format(arr))
+
+print("My number : {}".format(my_number))
+
+print("My index : {}".format(index))
